694a66836a533871157f6af9b8f83745585f19c7
Rainer Villido <Rainer.Villido@nortal.com>
2018-06-13 14:41:24 +0300

    Refactored test database

---
+++ src/test/java/ee/nortal/tdd/TestDatabase.java
@@ -0,0 +1,51 @@
+package ee.nortal.tdd;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+public class TestDatabase {
+
+  private static EmbeddedDatabase dataSource;
+  private static SimpleJdbcInsert userInsert;
+
+  public static EmbeddedDatabase setUp() {
+    dataSource = buildTestDatabase();
+    userInsert = new SimpleJdbcInsert(dataSource).withTableName("MOBILE_USER");
+    generateTestData();
+    return dataSource;
+  }
+
+  public static void tearDown() {
+    dataSource.shutdown();
+  }
+
+  private static EmbeddedDatabase buildTestDatabase() {
+    EmbeddedDatabase embeddedDatabase = new EmbeddedDatabaseBuilder()
+            .generateUniqueName(true)
+            .setType(EmbeddedDatabaseType.HSQL)
+            .setScriptEncoding("UTF-8")
+            .ignoreFailedDrops(true)
+            .addScript("sql-scripts/user-schema.sql")
+            .build();
+    return embeddedDatabase;
+  }
+
+  public static void addUser(String name, String idCode) {
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("PERSON_ID_CODE", idCode);
+    params.addValue("FULL_NAME", name);
+    userInsert.execute(params);
+  }
+
+  private static void generateTestData() {
+    addUser("Natalie Jones ", "4020203040992582");
+    addUser("Ben Richards", "301020304023592");
+    addUser("Mikaela Banes ", "423020304023592");
+    addUser("Julius Benedict", "301020304012053");
+    addUser("Jack Slater ", "3010203040954124");
+    addUser("Harry Tasker", "3010203040992582");
+  }
+}
--- src/test/java/ee/nortal/tdd/dao/MobileUserDaoTest.java
+++ src/test/java/ee/nortal/tdd/dao/MobileUserDaoTest.java
@@ -1,15 +1,13 @@
 package ee.nortal.tdd.dao;
 
+import ee.nortal.tdd.TestDatabase;
 import ee.nortal.tdd.signature.UserNotFoundException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import static ee.nortal.tdd.TestDatabase.addUser;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -18,46 +16,16 @@ public class MobileUserDaoTest {
   private final MobileUserDaoImpl mobileUserDao = new MobileUserDaoImpl();
 
   private EmbeddedDatabase dataSource;
-  private SimpleJdbcInsert userInsert;
 
   @Before
   public void setUp() {
-    dataSource = buildTestDatabase();
-    userInsert = new SimpleJdbcInsert(dataSource).withTableName("MOBILE_USER");
-    generateTestData();
+    dataSource = TestDatabase.setUp();
     mobileUserDao.setDataSource(dataSource);
   }
 
   @After
   public void tearDown() {
-    dataSource.shutdown();
-  }
-
-  private static EmbeddedDatabase buildTestDatabase() {
-    EmbeddedDatabase embeddedDatabase = new EmbeddedDatabaseBuilder()
-            .generateUniqueName(true)
-            .setType(EmbeddedDatabaseType.HSQL)
-            .setScriptEncoding("UTF-8")
-            .ignoreFailedDrops(true)
-            .addScript("sql-scripts/user-schema.sql")
-            .build();
-    return embeddedDatabase;
-  }
-
-  public void addUser(String name, String idCode) {
-    MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue("PERSON_ID_CODE", idCode);
-    params.addValue("FULL_NAME", name);
-    userInsert.execute(params);
-  }
-
-  private void generateTestData() {
-    addUser("Natalie Jones ", "4020203040992582");
-    addUser("Ben Richards", "301020304023592");
-    addUser("Mikaela Banes ", "423020304023592");
-    addUser("Julius Benedict", "301020304012053");
-    addUser("Jack Slater ", "3010203040954124");
-    addUser("Harry Tasker", "3010203040992582");
+    TestDatabase.tearDown();
   }
 
   @Test(expected = UserNotFoundException.class)
@@ -72,4 +40,5 @@ public class MobileUserDaoTest {
     assertNotNull(user);
     assertEquals("John Matrix", user.getFullName());
   }
+
 }
